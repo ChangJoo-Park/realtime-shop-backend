79ff0db9359b56fc4ecd06eae167ee5c74f64ca6
ChangJoo Park <pcjpcj2@gmail.com>
2018-03-19 00:36:12 +0900

    Add orders, customer routes to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,11 @@
+import { orders } from './orders';
+import { customers } from './customers';
 import { categories } from './categories'
 import { products } from './products'
 
 export default [
   ...categories,
-  ...products
+  ...products,
+  ...customers,
+  ...orders
 ]
