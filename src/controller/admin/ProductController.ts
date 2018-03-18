caae7582ce6c8c68e378013295e9e14e08cc4fcc
ChangJoo Park <pcjpcj2@gmail.com>
2018-03-18 10:51:02 +0900

    Format Product controller

--- src/controller/admin/ProductController.ts
+++ src/controller/admin/ProductController.ts
@@ -1,64 +1,44 @@
-import {Request, Response} from "express";
-import {getManager} from "typeorm";
-import {Product} from "../../entity/Product";
+import { Request, Response } from "express";
+import { getManager } from "typeorm";
+import { Product } from "../../entity/Product";
 
 /**
  * Loads all posts from the database.
  */
 export async function index(request: Request, response: Response) {
-  console.log('products')
-    // get a post repository to perform operations with post
-    response.render('products/index', {
-      title: 'Products',
-      page_name: 'products'
-    })
+  const products = Product.find()
+
+  response.render('products/index', {
+    title: 'All Products',
+    page_name: 'products',
+    products: products
+  })
 }
 
 
 export async function show(request: Request, response: Response) {
-  // get a post repository to perform operations with post
-  const productRepository = getManager().getRepository(Product);
-
-  // load a post by a given post id
-  const products = await productRepository.find();
   response.render('products/show', {
-      title: 'Categories',
-      page_name: 'Product - Show'
+    title: 'Product',
+    page_name: 'products'
+  })
+}
+
+export async function newProduct(request: Request, response: Response) {
+}
+
+
+export async function edit(request: Request, response: Response) {
+  response.render('products/edit', {
+    title: 'Update Product',
+    page_name: 'products'
   })
 }
 
 export async function create(request: Request, response: Response) {
-  // get a post repository to perform operations with post
-  const productRepository = getManager().getRepository(Product);
-
-  // load a post by a given post id
-  const products = await productRepository.find();
-  response.render('products/new', {
-      title: 'Categories',
-      page_name: 'Product - New'
-  })
 }
 
-export async function edit(request: Request, response: Response) {
-  // get a post repository to perform operations with post
-  const productRepository = getManager().getRepository(Product);
-
-  // load a post by a given post id
-  const products = await productRepository.find();
-  response.render('products/edit', {
-      title: 'Categories',
-      page_name: 'Product - Edit'
-  })
+export async function update(request: Request, response: Response) {
 }
 
 export async function destroy(request: Request, response: Response) {
-  // get a post repository to perform operations with post
-  const productRepository = getManager().getRepository(Product);
-
-  // load a post by a given post id
-  const products = await productRepository.find();
-  response.render('products/edit', {
-      title: 'Categories',
-      page_name: 'Product - Edit'
-  })
 }
