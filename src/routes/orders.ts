e5243a091b964954ca3c9ffae166e30224ac3da7
ChangJoo Park <pcjpcj2@gmail.com>
2018-03-19 00:36:03 +0900

    Add orders route

---
+++ src/routes/orders.ts
@@ -0,0 +1,39 @@
+import * as order from "../controller/admin/OrderController";
+
+export const orders = [
+  {
+    path: "/admin/orders",
+    method: "get",
+    action: order.index
+  },
+  {
+    path: "/admin/orders/new",
+    method: "get",
+    action: order.newProduct
+  },
+  {
+    path: "/admin/orders",
+    method: "post",
+    action: order.create
+  },
+  {
+    path: "/admin/orders/:id",
+    method: "get",
+    action: order.show
+  },
+  {
+    path: "/admin/orders/:id/edit",
+    method: "get",
+    action: order.edit
+  },
+  {
+    path: "/admin/orders/:id",
+    method: "put",
+    action: order.update
+  },
+  {
+    path: "/admin/orders/:id",
+    method: "delete",
+    action: order.destroy
+  }
+]
