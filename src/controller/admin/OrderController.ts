f53ec56973a7385e7de26e2a11b9e7503ff69c36
ChangJoo Park <pcjpcj2@gmail.com>
2018-03-22 22:49:46 +0900

    Add create order with Customer and LineItems

--- src/controller/admin/OrderController.ts
+++ src/controller/admin/OrderController.ts
@@ -1,3 +1,4 @@
+import { LineItem } from './../../entity/LineItem';
 import { Product } from './../../entity/Product';
 import { Customer } from './../../entity/Customer';
 
@@ -8,7 +9,7 @@ import { Order } from "../../entity/Order";
 const page_name = 'orders'
 
 export async function index(request: Request, response: Response) {
-  const orders = await Order.find({relations: ['customer', 'lineItems']})
+  const orders = await Order.find({relations: ['customer', 'lineItems', 'lineItems.product']})
   response.render('orders/index', {
     title: 'All Orders',
     page_name,
@@ -53,15 +54,27 @@ export async function edit(request: Request, response: Response) {
 }
 
 export async function create(request: Request, response: Response) {
-  const { name, description, customer, lineItems } = request.body
-  console.log(lineItems)
-  // const order = new Order()
-  // order.customer = await Customer.findOneById(customer)
-  // order.lineItems = await Product.findByIds(getProductArray(products))
+  const { name, description, customer, lineItems = [] } = request.body
+  const flattenItems = flatLineItems(lineItems)
+  const lineItemIdx = flattenItems.map(f => f.product)
+  const products = await Product.findByIds(lineItemIdx)
 
-  // await order.save()
+  // FIXME: 뭔가 한번에 할 수 있을거같은데..
+  let newLineItems = []
+  await asyncForEach(products, async (p, index) => {
+    const item = new LineItem()
+    item.product = p
+    item.amount = flattenItems[index].amount
+    await item.save()
+    newLineItems.push(item)
+  })
 
-  // response.redirect('/admin/orders')
+  const order = new Order()
+  order.customer = await Customer.findOneById(customer)
+  order.lineItems = newLineItems
+  const o = await order.save()
+
+  response.redirect('/admin/orders')
 }
 
 export async function update(request: Request, response: Response) {
@@ -77,4 +90,25 @@ export async function destroy(request: Request, response: Response) {
   await order.remove()
 
   response.redirect('/admin/orders')
+}
+
+const flatLineItems = (lineItems) => {
+  const flattenItems = []
+
+  lineItems.forEach(li => {
+    const existsIndex = flattenItems.findIndex(fi => li.product === fi.product)
+    if (existsIndex >= 0) {
+      flattenItems[existsIndex].amount += (li.amount * 1)
+    } else {
+      li.amount = (li.amount * 1)
+      flattenItems.push(li)
+    }
+  })
+  return flattenItems
+}
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array)
+  }
 }
